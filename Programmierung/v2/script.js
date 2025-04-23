3b5d1e1877d499cbbf79f48e2392eede40870440
az8118 <arminz9963@gmail.com>
2025-04-23 16:56:09 +0200

    Schnitt Bereiche werden jz hinzugefügt nach Button Klick, keine Funktionalität!

--- Programmierung/v2/script.js
+++ Programmierung/v2/script.js
@@ -15,3 +15,28 @@ VideoInput.addEventListener('change', function () {
     VideoPlayer.style.display = "block"; // Zeigt den Video-Player an
     VideoInputLabel.style.display = "none"; // Versteckt das Label
 });
+
+const addNewCutBtn = document.getElementById('addNewCutBtn');
+let cutSectionCount = 0
+const SchnittContainer = document.getElementById('SchnittContainer');
+
+addNewCutBtn.addEventListener('click', function () {
+    cutSectionCount++; // Erhöht den Zähler für die Schnitte
+
+    const cutSection = document.createElement('div');
+    cutSection.id = `Schnitt${cutSectionCount}`; // Setzt die ID für den neuen Schnitt-Container
+    cutSection.innerHTML = `
+    <h3>Schnitt ${cutSectionCount}</h3>
+    <label>Trimmen:</label>
+    <input type="range" class="startRange" min="0" step="0.01" style="width: 40%" value="0"/>
+    <input type="range" class="endRange" min="0" step="0.01" style="width: 40%" value="0"/>
+    <br />
+    <span>Start: <input type="text" class="startInput" maxlength="8" value="00:00.00" /></span>
+    <span style="margin-left: 20px">Ende: <input type="text" class="endInput" maxlength="8" value="00:00.00" /></span>
+    <br /><br />
+  `;
+
+    SchnittContainer.append(cutSection); // Fügt den neuen Schnitt-Container hinzu
+
+
+});
