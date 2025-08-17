8200f9893eba5b0c79f8f5b230e0e2ce108fe01c
arminz9963 <arminz9963@gmail.com>
2025-08-17 19:37:22 +0200

    test result durch neue inferenz viel besser! W

--- inference/inference_neu.py
+++ inference/inference_neu.py
@@ -3,11 +3,11 @@ from llama_cpp import Llama
 import json
 start_time = time.time()
 llm = Llama(
-    model_path="models/model_v4.Q4_K_m.gguf",
-    n_ctx=15000,
+    model_path="models/model_v1.Q4_K_M.gguf",
+    n_ctx=22000,
     verbose=False
 )
-with open("data/testdaten3.json", "r", encoding="utf-8") as file:
+with open("data/testdaten1.json", "r", encoding="utf-8") as file:
     data = json.load(file)
     for i, dataset in enumerate(data):
         print("########################################################################")
@@ -17,9 +17,9 @@ with open("data/testdaten3.json", "r", encoding="utf-8") as file:
         answer = output['choices'][0]['message']['content'].strip()
         print(f"Ausgabe: {answer}")
         print(f"Lösung: {dataset['conversations'][1]['value']}")
-        with open("tests/neu/test_result_v4_n.txt", "a", encoding="utf-8") as datei:
+        with open("tests/neu/test_result_v1_n.txt", "a", encoding="utf-8") as datei:
             datei.writelines(f"Test {i+1}\nAusgabe: {answer}\nLösung: {dataset['conversations'][1]['value']}\n")
 total_time = time.time() - start_time
 print(f"Gesamtzeit: {total_time:.2f} Sekunden")
-with open("tests/neu/test_result_v4_n.txt", "a", encoding="utf-8") as datei:
+with open("tests/neu/test_result_v1_n.txt", "a", encoding="utf-8") as datei:
     datei.writelines(f"Gesamtzeit: {total_time}")
