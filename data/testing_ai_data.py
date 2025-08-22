ed8ad1943821933e2068f816a745db2ff216319d
az8118 <arminz9963@gmail.com>
2025-08-22 21:02:48 +0200

    neue Daten dank gemini 2.5 pro!!

---
+++ data/testing_ai_data.py
@@ -0,0 +1,38 @@
+import json
+import re
+
+data_file = "data/trainingsdaten3_50.json"
+
+with open(data_file, 'r', encoding='utf-8') as datei:
+    data = json.load(datei)
+
+for num, entry in enumerate(data):
+    final_text = []
+    human = entry["conversations"][0]["value"]
+    schnitte = entry["conversations"][1]["value"]
+    schnitte = eval(schnitte)
+
+    match = re.search(r'Transkript: (.*)', human, re.DOTALL)
+    if match:
+        transkript = match.group(1).strip()
+
+    transkript_list = transkript.split("]")
+
+    transkript_list = [wort.replace("[", "") for wort in transkript_list if wort != "" ]
+
+    for wort_complex in transkript_list:
+        wort, start, end = wort_complex.rsplit(" ", 2)
+        start = start.replace(",", "")
+        start = float(start)
+        end = float(end)
+
+        for cut_start, cut_end in schnitte:
+            if start >= cut_start and end <= cut_end:
+                final_text.append(wort)
+                break
+
+    final_text = " ".join(final_text)
+    with open("approve_ai_data.txt", "a", encoding="utf-8") as output_file:
+        output_file.write(final_text + "\n")
+    
+print(num+1)
