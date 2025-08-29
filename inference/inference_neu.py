9108fcb979b957abbce7807e668317d10924587c
arminz9963 <arminz9963@gmail.com>
2025-08-29 23:13:20 +0200

    inferenz v11 fail

--- inference/inference_neu.py
+++ inference/inference_neu.py
@@ -12,18 +12,19 @@ start_time = time.time()
 
 
 llm = Llama(
-    model_path="models/model_v9.Q4_K_M.gguf",
+    model_path="models/model_v11.Q4_K_M.gguf",
     n_ctx=15000,
     verbose=False
 )
 
-with open("data/testdaten3_v2.json", "r", encoding="utf-8") as file:
+with open("data/testdaten3_v2_alpaca.json", "r", encoding="utf-8") as file:
     data = json.load(file)
     for i, dataset in enumerate(data):
         print("########################################################################")
         print("Test Nummer:", i + 1)
         system = "Du bist ein Video-Editing Assistant. Analysiere das folgende Transkript und gib passende Schnittvorschläge zurück, beachte hierbei die mitgegebene Beschreibung."
-        prompt = f"{dataset['conversations'][0]['value']}\n"
+        #prompt = f"{dataset['conversations'][0]['value']}\n"
+        prompt = f"{dataset['instruction']} \n{dataset['input']}"
         message = [
             {"role": "system", "content": system},
             {"role": "user", "content": prompt}
@@ -31,10 +32,10 @@ with open("data/testdaten3_v2.json", "r", encoding="utf-8") as file:
         output = llm.create_chat_completion(messages=message)
         answer = output['choices'][0]['message']['content'].strip()
         print(f"Ausgabe: {answer}")
-        print(f"Lösung: {dataset['conversations'][1]['value']}")
-        with open("tests/neu/test_result_v9_2.txt", "a", encoding="utf-8") as datei:
-            datei.writelines(f"Test {i+1}\nAusgabe: {answer}\nLösung: {dataset['conversations'][1]['value']}\n")
+        print(f"Lösung: {dataset['response']}")
+        with open("tests/neu/test_result_v11.txt", "a", encoding="utf-8") as datei:
+            datei.writelines(f"Test {i+1}\nAusgabe: {answer}\nLösung: {dataset['response']}\n")
 total_time = time.time() - start_time
 print(f"Gesamtzeit: {total_time:.2f} Sekunden")
-with open("tests/neu/test_result_v9_2.txt", "a", encoding="utf-8") as datei:
+with open("tests/neu/test_result_v11.txt", "a", encoding="utf-8") as datei:
     datei.writelines(f"Gesamtzeit: {total_time}")
