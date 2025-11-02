f174da56b855bce52da6a08ccc375c0448ebd248
az8118 <arminz9963@gmail.com>
2025-11-02 22:11:36 +0100

    minor

--- inference/inference.py
+++ inference/inference.py
@@ -12,12 +12,12 @@ start_time = time.time()
 
 
 llm = Llama(
-    model_path="models/model_v11.Q4_K_M.gguf",
+    model_path="models/model_v1.Q4_K_M.gguf",
     n_ctx=15000, # Maximale Kontextlänge
     verbose=False
 )
 
-with open("data/testdaten3_v2_alpaca.json", "r", encoding="utf-8") as file:
+with open("data/testdaten1.json", "r", encoding="utf-8") as file:
     data = json.load(file)
     for i, dataset in enumerate(data):
         print("########################################################################")
@@ -34,9 +34,9 @@ with open("data/testdaten3_v2_alpaca.json", "r", encoding="utf-8") as file:
         answer = output['choices'][0]['message']['content'].strip()
         print(f"Ausgabe: {answer}")
         print(f"Lösung: {dataset['response']}")
-        with open("tests/neu/test_result_v11.txt", "a", encoding="utf-8") as datei:
+        with open("tests/test_result_v1.txt", "a", encoding="utf-8") as datei:
             datei.writelines(f"Test {i+1}\nAusgabe: {answer}\nLösung: {dataset['response']}\n")
 total_time = time.time() - start_time
 print(f"Gesamtzeit: {total_time:.2f} Sekunden")
-with open("tests/neu/test_result_v11.txt", "a", encoding="utf-8") as datei:
+with open("tests/test_result_v1.txt", "a", encoding="utf-8") as datei:
     datei.writelines(f"Gesamtzeit: {total_time}")
